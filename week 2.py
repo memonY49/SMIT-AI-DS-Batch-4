6c4c0374c7ead76d5987fff8d76e9672d9adbbff
memonY49 <memonY49@gmail.com>
2026-02-03 17:54:46 +0500

    update

---
+++ week 2.py
@@ -0,0 +1,52 @@
+'''
+Implicit: Automatic conversion (int to float)
+Explicit: Forced to be converted (float to int)
+convert anything to integer use int(data/variable) function
+convert anything to str use str(data/variable) function
+convert anything to float use float(data/variable) function
+'''
+a = 4.6
+b = int(a)
+
+# to check type of any value use type(value) function
+
+
+#create 2 variables named as X,Y. perform (+,-,*,/ and %) on them.
+x = 6
+y = 4
+
+print(x,"+",y,"=",x+y,end= ' ')
+print(x,"-",y,"=",x-y, sep = ' ')
+print(x,"/",y,"=",x/y)
+print(x,"*",y,"=",x*y)
+print(x,"%",y,"=",x%y)
+print(y,"%",x,"=",y%x)
+
+
+
+
+
+
+
+
+'''
+comparison operators:
+1. less than '<'
+2. greater than '>'
+3. equal to '=='
+4. not equal to '!='
+5. less than or equal to '<='
+6. greater than or equal to '>='
+'''
+x = 7
+y = 5
+
+print(x<y)
+print(x>y)
+print(x<=y)
+print(x>=y)
+print(x==y)
+print(x!=y)
+
+
+
