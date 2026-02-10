ecdbecffb1e71a1881bb792e2b978b99e19756cd
memonY49 <memonY49@gmail.com>
2026-02-10 18:48:37 +0500

    update

---
+++ week 3.py
@@ -0,0 +1,29 @@
+#message = "this is a class of \\n \"AI and Data Science\""
+
+#print(message)
+
+
+# to use any string function:
+# "string".func()
+
+message = "675237625637452381 "
+
+#print(message.capitalize())
+#print(message.title())
+#print(message.lower())
+#print(message.upper())
+new = message.upper()
+#print(new.count('N'))
+#print(message.startswith('my'))
+#print(message.find('ae'))
+#print(message.index('ae'))
+#print(message.isalpha())
+#print(message.isalnum())
+a = message.isnumeric()
+name = "Ahmed"
+Class = "DM"
+message = f"{name} is student of {Class} class"
+print(message)
+#print(message.format(cl = Class,name = name))
+
+print(r"Hello \n world".swapcase())
