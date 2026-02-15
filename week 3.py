61f99b559c151a4857234a3048c0ed3e2439f082
memonY49 <memonY49@gmail.com>
2026-02-15 18:29:39 +0500

    update

--- week 3.py
+++ week 3.py
@@ -23,7 +23,80 @@ a = message.isnumeric()
 name = "Ahmed"
 Class = "DM"
 message = f"{name} is student of {Class} class"
-print(message)
+#print(message)
 #print(message.format(cl = Class,name = name))
 
-print(r"Hello \n world".swapcase())
+#print(r"Hello \n world".swapcase())
+
+
+data = "yasir nawaz memon 03000000000 4130987872667"
+
+#slicing
+#print(data.split(' ')[3])
+
+#print(data.strip())
+#print(data.lstrip())
+#print(data.replace("a","A"))
+
+
+
+x = 6
+y = 3
+
+if x>y and x>5:
+    #body of if
+    print(f"x={x} is greater than y={y}")
+else:
+    print(f"x={x} is less than y={y}")
+
+# create a variable named as number and get a number from user using input()
+# print if the number is even or odd (hint: number % 2 == 0)
+
+number = int(input("Enter your number to check: "))
+if number % 2 == 0:
+    print(f"The number = {number} is even")
+else:
+    print(f"The number = {number} is odd")
+    
+
+
+
+
+
+
+
+# task 3
+# step 1: create 5 variables named as (Name,Fname,phone,email and pass)
+# step 2: create 2 more variables named as (useremail and userpass) and get
+#         email and password from the user using input()
+# step 3: create a condition if email matches from the email variable
+#         print user found else user not found!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
