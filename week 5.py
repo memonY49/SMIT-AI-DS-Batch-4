8075314173abb2eaedbab97f590134569d96617c
memonY49 <memonY49@gmail.com>
2026-02-24 15:10:16 +0500

    update

--- week 5.py
+++ week 5.py
@@ -5,6 +5,46 @@ Type: for loop
         for counter in range(0,10):
             #body of for loop
 '''
-for counter in range(1,10):
-    #body of for loop
-    print('hello')
+
+#for i in range(1,11):
+    #print(9*i)
+
+
+#for odd in range(11,1,-1):
+#    print(odd)
+
+#a,b,c,d,e,f,g,h
+#for i in "abcdefgh":
+#    print(i,end = ',')
+
+'''
+    while loop:
+    syntax:
+        initialize counter
+        while condition:
+            body of while
+            inc/dec
+'''
+
+counter = 1
+while counter <=10:
+    print(counter)
+    counter = counter + 1
+
+
+
+print(counter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
