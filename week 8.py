d1adf70d16331552585edc4b6f1d05dd7719d23b
memonY49 <memonY49@gmail.com>
2026-03-31 23:40:57 +0500

    update

--- week 8.py
+++ week 8.py
@@ -38,7 +38,7 @@ syntax:
 
 
 
-mylist = [1,22,34,55,67,93,55]
+#mylist = [1,22,34,55,67,93,55]
 #mylist.append(56)
 #mylist.insert(7,'abc')
 #mylist.extend([22,66,78,28,39])
@@ -59,8 +59,126 @@ print(elements)
 '''
 
 #mylist.reverse()
-mylist.sort(reverse = True)
-print(mylist)
+#mylist.sort(reverse = True)
+#print(mylist)
+
+
+
+'''
+Multi Dimensional List: List of Lists
+syntax:
+    mylist = [[1,2,3],
+              ['a','b','c'],
+              [2.6,4.7,5.5],
+              [Ture,False,True]]
+    print(mylist[2][1])
+
+
+data = [['Yasir','abc@gmail.com','abc123'],
+        ['Yasir1','abc1@gmail.com','abc123'],
+        ['Yasir2','abc2@gmail.com','abc123'],
+        ['Yasir3','abc3@gmail.com','abc123']]
+
+for i in range(len(data)):
+    user = data[i]
+    for j in range(len(user)):
+        print(user[j])
+    print("*"*10)
+'''
+
+''' Class Task:-
+step1: Store atleast 5 user data (Name,Fname, Phone, Email and Pass) in a
+       single multi dimensional list.
+step2: Ask user for useremail and userpass.
+step3: Match if useremail and userpass matches to any of the stored user.
+step4: Print user matched along with the details of that user otherwise print
+       user not found.
+
+data = [['Yasir','Nawaz','abc@gmail.com','abc123'],
+        ['Yasir1','Nawaz','abc1@gmail.com','abc123'],
+        ['Yasir2','Nawaz','abc2@gmail.com','abc123'],
+        ['Yasir3','Nawaz','abc3@gmail.com','abc123']]
+
+useremail = input("Enter your email: ")
+userpass = input("Enter your pass: ")
+
+for i in range(len(data)):
+    
+    if useremail == data[i][2] and userpass == data[i][3]:
+        print("User Matched")
+        print("Name:",user[0])
+        print("FName:",user[1])
+        print("Email:",user[2])
+        break
+else:
+    print("User not found!!")
+'''
+a = [[[11,12,13],
+      ['a1','a2','a3'],
+      ['b1','b2','b3']],
+     [[21,22,23],
+      ['c1','c2','c3'],
+      ['d1','d2','d3']],
+     [[31,32,33],
+      ['e1','e2','e3'],
+      ['f1','f2','f3']]]
+
+print(a[0][1][2])
+
+'''
+data = [[['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price]],
+        [['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price]],
+        [['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price],
+          ['Company','Car name','Model','Year',Price]]]
+Task for week 8: showroom managment system.
+    step1: Store details of atleast five cars for atleast 3 companies in a
+           multi dimesional list.
+    step2: Create a menu for the user and ask for sell or buy the car.
+        menu:
+            1.Buy
+            2.Sell
+            0.Exit
+            Enter your selection:
+    step3: For Buy option show user another menu for the car companies.
+        menu:
+            1.Toyota
+            2.Suzuki
+            3.Kia
+            Enter your selection:
+    step4: if the user selects the company than show them all cars that are
+           available for sell on your showroom.
+        menu:
+            1.Corolla
+            2.Yaris
+            3.Grande
+            Enter your selection:
+    step5: if the user selects any car show tham all details about that car and
+           thanks for shopping message.
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
