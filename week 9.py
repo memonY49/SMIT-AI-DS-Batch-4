5eaf5446babdeba6ecc6b238ceaa2f01e1306e09
memonY49 <memonY49@gmail.com>
2026-04-12 18:35:24 +0500

    update

--- week 9.py
+++ week 9.py
@@ -92,7 +92,7 @@ print("topers:",top)
 2. Tic Tac Toe Board
     -> Represent board using 2D list
     -> Check winner
-"""
+
 
 board = [[" "," "," "],
          [" "," "," "],
@@ -152,7 +152,130 @@ while(moves<=9): #move count loop
             exit(0)
         #--------------
 print("draw.........")
+"""
+"""
+Dictionary: A collection of key and value pairs.
+ syntax:
+         mydict = {"key1":"value","key2":"value"}
+functions:
+    1. keys()
+    2. values()
+    3. items()
 
+mydict = {"name":"Yasir",
+          "fname":"Nawaz",
+          "phone":"03003000000",
+          "pass":True,
+          "marks":890}
+mydict['add'] = "hvdjcgascydgvcjwvjwhdm"
+
+sortedkeys = list(mydict.items())
+#sortedkeys.sort()
+#print(sortedkeys)
+
+for k,v in mydict.items():
+    print(f"{k} : {v}")
+
+
+data = [{"name":"Yasir",
+          "fname":"Nawaz",
+          "phone":"03003000000",
+          "pass":True,
+         'email':"yasir123@gmail.com",
+         'password':'abc123',
+          "marks":890},
+        {"name":"Ahmed",
+          "fname":"Faraz",
+          "phone":"03003000000",
+          "pass":True,
+         'email':"ahmed123@gmail.com",
+         'password':'abc123',
+          "marks":890},
+        {"name":"Ali",
+          "fname":"Parvez",
+          "phone":"03003000000",
+          "pass":True,
+         'email':"ali123@gmail.com",
+         'password':'abc123',
+          "marks":890}]
+
+useremail = input("Enter your email:")
+userpass = input("Enter your pass:")
+
+for user in data:
+    if useremail == user['email'] and userpass == user['password']:
+        print('*'*10)
+        for k,v in user.items():
+            print(f"{k} : {v}")
+        break
+else:
+    print("user not found!!")
+"""
+'''
+data = {"u1":{"name":"Yasir",
+             "fname":"Nawaz",
+             "phone":"03003000000",
+             "pass":True,
+             'email':"yasir123@gmail.com",
+             'password':'abc123',
+              "marks":890},
+        "u2":{"name":"Ahmed",
+            "fname":"Faraz",
+            "phone":"03003000000",
+            "pass":True,
+            'email':"ahmed123@gmail.com",
+            'password':'abc123',
+            "marks":890},
+        "u3":{"name":"Ali",
+            "fname":"Parvez",
+            "phone":"03003000000",
+            "pass":True,
+           'email':"ali123@gmail.com",
+           'password':'abc123',
+           "marks":890}}
+
+#print(data['u2']['name'])
+
+useremail = input("Enter your email:")
+userpass = input("Enter your pass:")
+
+for userid, uservalue in data.items():
+    if useremail == uservalue['email'] and userpass == uservalue['password']:
+        print('*'*10)
+        for k,v in uservalue.items():
+            print(f"{k} : {v}")
+        break
+else:
+    print("user not found!!")
+
+'''
+data = [{"name":"Yasir",
+          "fname":"Nawaz",
+          "phone":"03003000000",
+          "pass":True,
+         'email':"yasir123@gmail.com",
+         'password':'abc123',
+          "marks":890},
+        {"name":"Ahmed",
+          "fname":"Faraz",
+          "phone":"03003000000",
+          "pass":True,
+         'email':"ahmed123@gmail.com",
+         'password':'abc123',
+          "marks":890},
+        {"name":"Ali",
+          "fname":"Parvez",
+          "phone":"03003000000",
+          "pass":True,
+         'email':"ali123@gmail.com",
+         'password':'abc123',
+          "marks":890}]
+
+listnames = [(std['name'],std['email']) for std in data]
+print(listnames)
+
+#mylist = [x for x in range(1,101)]
+#print(mylist)
 
 
 
